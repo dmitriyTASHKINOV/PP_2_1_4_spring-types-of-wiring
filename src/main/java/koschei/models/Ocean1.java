733a5e5c6c503b90d8efb32d5f005d6ae1031d97
dmitriyTASHKINOV <tashkinov.mitiaaaa@gmail.com>
2024-07-03 19:03:38 +0300

    qwe

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,13 +1,17 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
+    @Qualifier("getIsland")
     @Autowired
-    private Island2 island;
+    private Island2 island ;
+
+
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Wood3 {
-
+    @Autowired
     private final Rabbit4 rabbit;
 
-    @Autowired
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
